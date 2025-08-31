037c9db77c0c62d776be2d59b9d0fa81b91ebe3d
Chris Hahn <git@chrishahn.net>
2025-08-31 12:31:39 -0400

    Add "getComputerChoice" function

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+  const num = Math.floor(Math.random() * 3)
+  switch (num) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+}
