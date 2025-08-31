7a6e87e6cb2346b5aa8c8f3bf3ffa9a5e83bdf8a
Chris Hahn <git@chrishahn.net>
2025-08-31 14:59:01 -0400

    Add "playRound" function

--- script.js
+++ script.js
@@ -17,3 +17,19 @@ function getHumanChoice () {
   const choice = prompt("Rock, paper, or scissors?");
   return choice.toLowerCase();
 }
+
+function playRound (humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    return `Draw: you both chose ${humanChoice}`;
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    return `You win: ${humanChoice} beats ${computerChoice}.`;
+  } else {
+    computerScore++;
+    return `You lose: ${computerChoice} beats ${humanChoice}.`;
+  }
+}
