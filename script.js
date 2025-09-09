355a4060d5832514bb77f4dcead19c8f33a16e14
Chris Hahn <git@chrishahn.net>
2025-09-09 09:20:02 -0400

    Add playRound() event listener to buttons

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function playGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  for (let game = 0; game < 5; game++) {
+  // for (let game = 0; game < 5; game++) {
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
     const result = playRound(humanSelection, computerSelection);
@@ -51,7 +51,7 @@ function playGame() {
         console.log(`You lose: ${computerSelection} beats ${humanSelection}.`);
         break;
     }
-  }
+  // }
 
   if (humanScore > computerScore) {
     console.log(`You won the game! Your score: ${humanScore}, computer score: ${computerScore}`);
@@ -60,4 +60,12 @@ function playGame() {
   }
 }
 
-playGame();
+const div = document.querySelector("#results");
+const btns = document.querySelectorAll("button");
+
+for (const btn of btns) {
+  btn.addEventListener("click", e => {
+    humanChoice = e.target.textContent.toLowerCase();
+    playRound(humanChoice, getComputerChoice());
+  });
+}
